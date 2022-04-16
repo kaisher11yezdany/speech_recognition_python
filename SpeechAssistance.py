f129781fcadd52e2096d8c1e2d690959c80a3244
Kaisher Yezdany <kasher11yezdany@gmail.com>
2022-04-16 17:59:50 +0530

    first commit

---
+++ SpeechAssistance.py
@@ -0,0 +1,22 @@
+import speech_recognition as sr
+import speech_recognition as sr
+import os
+import sys
+import re
+import webbrowser
+import smtplib
+import requests
+import subprocess
+from pyowm import OWM
+import youtube_dl
+import vlc
+import urllib
+import urllib2
+import json
+from bs4 import BeautifulSoup as soup
+from urllib2 import urlopen
+import wikipedia
+import random
+from time import strftime
+
+import speech_recognition
